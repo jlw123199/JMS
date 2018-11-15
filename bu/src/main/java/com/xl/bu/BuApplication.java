2db75dc814e0e54bc41b87c8b53c4658231b58b4
jia.liwei <jia.liwei@rd.com>
2018-11-15 15:07:53 +0800

    Add project code

---
+++ bu/src/main/java/com/xl/bu/BuApplication.java
@@ -0,0 +1,12 @@
+package com.xl.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuApplication.class, args);
+    }
+}
