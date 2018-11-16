ebf881f04c3480cf6e64c18b58d132822e32c41f
jia.liwei <jia.liwei@rd.com>
2018-11-16 10:53:58 +0800

    add post request example

--- bu/order-service/src/main/java/com/xl/orderservice/OrderController.java
+++ bu/order-service/src/main/java/com/xl/orderservice/OrderController.java
@@ -1,6 +1,8 @@
 package com.xl.orderservice;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xl.orderservice.modules.OrderModel;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -30,8 +32,23 @@ public class OrderController {
         }
     }
 
+    @RequestMapping(value = "/setValueWithPost" ,method = RequestMethod.POST)
+    public String writeRedisWithPost( @RequestBody OrderModel orderModel){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            stringRedisTemplate.opsForValue().set( orderModel.getOrderId() , mapper.writeValueAsString(orderModel));
+            return  stringRedisTemplate.opsForValue().get( orderModel.getOrderId() );
+        }
+        catch (Exception ex){
+            return ex.getMessage();
+        }
+    }
+
     @RequestMapping(value = "/getValue" ,method = RequestMethod.GET)
-    public String readRedis( @RequestParam String redisKey ){
+    public String readRedis( String redisKey ){
         return  stringRedisTemplate.opsForValue().get( redisKey );
     }
+
+
 }
---
+++ bu/order-service/src/main/java/com/xl/orderservice/modules/OrderModel.java
@@ -0,0 +1,34 @@
+package com.xl.orderservice.modules;
+
+public class OrderModel {
+    String orderId;
+    String orderCode;
+    String orderType;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+
+
+}
