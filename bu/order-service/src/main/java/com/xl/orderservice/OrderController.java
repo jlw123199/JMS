4e9b6a387c3b8dc69dae9b729d4bb2872a84f05d
jia.liwei <jia.liwei@rd.com>
2018-11-16 10:01:56 +0800

    Add feature: access data from local redis

--- bu/order-service/src/main/java/com/xl/orderservice/OrderController.java
+++ bu/order-service/src/main/java/com/xl/orderservice/OrderController.java
@@ -2,18 +2,36 @@ package com.xl.orderservice;
 
 
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/order")
 @RestController
 public class OrderController {
     private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
 
     @RequestMapping(value = "/ping" ,method = RequestMethod.GET)
     public String verify(){
         return "Pong";
+    }
+
+    @RequestMapping(value = "/setValue" ,method = RequestMethod.GET)
+    public String writeRedis( @RequestParam String redisKey, @RequestParam String redisValue ){
+        try {
+            stringRedisTemplate.opsForValue().set( redisKey , redisValue);
+            return  stringRedisTemplate.opsForValue().get( redisKey );
+        }
+        catch (Exception ex){
+            return ex.getMessage();
+        }
+    }
+
+    @RequestMapping(value = "/getValue" ,method = RequestMethod.GET)
+    public String readRedis( @RequestParam String redisKey ){
+        return  stringRedisTemplate.opsForValue().get( redisKey );
     }
 }
